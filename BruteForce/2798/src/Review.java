96ff034ec2caa33acc5f48b6ef0a4daee2416ebd
thesalmonlover <gsj0919@naver.com>
2022-07-28 20:11:04 +0900

    recursion and bruteforce problems are re-solved

--- BruteForce/2798/src/Review.java
+++ BruteForce/2798/src/Review.java
@@ -2,39 +2,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Review {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(tk.nextToken());
-        int M = Integer.parseInt(tk.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        tk = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(tk.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int result = bruteForce(N, M, arr);
+        int result = bruteforce(N, M, arr);
         System.out.println(result);
-
     }
 
-    static int bruteForce(int N, int M, int[] arr) {
+    static int bruteforce(int N, int M, int[] arr) {
         int result = 0;
         for (int i = 0; i < N - 2; i++) {
             for (int j = i + 1; j < N - 1; j++) {
                 for (int k = j + 1; k < N; k++) {
                     int temp = arr[i] + arr[j] + arr[k];
-
                     if (temp == M)
                         return temp;
-
-                    if (result < temp && temp < M) {
+                    if (temp > result && temp < M)
                         result = temp;
-                    }
-
                 }
             }
         }
         return result;
     }
+
 }
