13fac1422c26e7e422f88ac5644a8daf72642f99
ChonkySeeal <gsi0919@naver.com>
2022-08-09 01:06:30 +0900

    counting sort is studied

---
+++ Sorting/CountingSort/src/CountingSort.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class CountingSort {
+
+    void countSort(int arr[], int n) {
+        int[] output = new int[n + 1];
+
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        int[] count = new int[max + 1];
+
+        for (int i = 0; i < max; i++) {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
+        }
+
+        for (int i = 1; i <= max; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[arr[i]] - 1] = arr[i];
+            count[arr[i]]--;
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] data = { 4, 2, 2, 8, 3, 3, 1 };
+        int size = data.length;
+        CountingSort cs = new CountingSort();
+        cs.countSort(data, size);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+    }
+}
---
+++ Sorting/RadixSort/src/RadixSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class RadixSort {
+
+    static int getMax(int arr[], int n) {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+
+    static void countSort(int arr[], int n, int exp) {
+        int output[] = new int[n];
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+
+        for (i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
