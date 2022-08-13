5a8c813bcc78c743b59f7911fdb2c6f0680c7a76
ChonkySeeal <gsi0919@naver.com>
2022-08-14 06:22:06 +0900

    problem 10989 is solved. quicksort is reviewed

--- Sorting/QuickSort/src/QuickSort.java
+++ Sorting/QuickSort/src/QuickSort.java
@@ -1,46 +1,42 @@
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = { 10, 7, 8, 9, 1, 5 };
-        int n = arr.length;
-
-        quicksort(arr, 0, n - 1);
-        System.out.println("Sorted array: ");
-        printArray(arr, n);
-    }
-
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    static void printArray(int[] arr, int size) {
-        for (int i = 0; i < size; i++)
+        quicksort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    static void quicksort(int[] arr, int left, int right) {
-        if (right - left <= 0)
-            return;
-        else {
-            int pivot = partition(arr, left, right);
-            quicksort(arr, left, pivot - 1);
-            quicksort(arr, pivot + 1, right);
         }
-
     }
 
-    static int partition(int[] arr, int left, int right) {
-        int pivot = arr[right];
-        int i = left - 1;
-        for (int j = left; j <= right - 1; j++) {
-            if (arr[j] < pivot) {
+    static int partition(int[] arr, int l, int r) {
+        int pivot = arr[r];
+        int i = l - 1;
+
+        for (int j = l; j < r; j++) {
+            if (arr[j] <= pivot) {
                 i++;
-                swap(arr, j, i);
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+
             }
+
         }
-        swap(arr, i + 1, left);
+
+        int temp = arr[i + 1];
+        arr[i + 1] = pivot;
+        arr[r] = temp;
+
         return i + 1;
+
     }
+
+    static void quicksort(int[] arr, int l, int r) {
+        if (l < r) {
+            int pivot = partition(arr, l, r);
+            quicksort(arr, l, pivot - 1);
+            quicksort(arr, pivot + 1, r);
+
+        }
+    }
+
 }
