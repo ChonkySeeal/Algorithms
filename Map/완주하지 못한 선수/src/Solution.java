b04132a5bcfcbebcc67770bb122880de6b656d2c
ChonkySeeal <gsi0919@naver.com>
2022-09-09 22:56:49 +0900

    완주하지 못한 선수 is solved

---
+++ Map/완주하지 못한 선수/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        String ans = "";
+        for (int i = 0; i < participant.length; i++) {
+            hm.put(participant[i], hm.getOrDefault(participant[i], 0) + 1);
+        }
+        for (int i = 0; i < completion.length; i++) {
+            hm.put(completion[i], hm.get(completion[i]) - 1);
+        }
+        for (int i = 0; i < participant.length; i++) {
+            if (hm.get(participant[i]) != 0) {
+                ans = participant[i];
+                break;
+            }
+        }
+        return ans;
+    }
+
+}
