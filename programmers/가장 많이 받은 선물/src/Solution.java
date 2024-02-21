fbaaf2ca4646abc0b5aa90e3b314d51edf7e07c8
ChonkySeeal <gsi0919@naver.com>
2024-02-21 22:03:24 +0900

    solved

--- programmers/가장 많이 받은 선물/src/Solution.java
+++ programmers/가장 많이 받은 선물/src/Solution.java
@@ -2,17 +2,32 @@ import java.util.HashMap;
 
 public class Solution {
     public int solution(String[] friends, String[] gifts) {
-        HashMap<String, Integer> names = new HashMap<>();
-        for(String friend : friends) {
-            names.put(friend, 0);
+        HashMap<String, Integer> map = new HashMap<>();
+        int[] count = new int[friends.length];
+        int[][] exchange = new int[friends.length][friends.length];
+
+        for(int i = 0;i < friends.length; i++) {
+            map.put(friends[i], i);
         }
 
         for(String gift : gifts) {
-            String[] giverNtaker = gift.split(" ");
-            names.put(giverNtaker[0], names.get(giverNtaker[0])+1);
+            String[] c = gift.split(" ");
+            count[map.get(c[0])]++;
+            count[map.get(c[1])]--;
+            exchange[map.get(c[0])][map.get(c[1])]++;
         }
 
         int answer = 0;
-        return answer;
+        for(int i =0; i < friends.length; i++) {
+            int cnt = 0;
+            for(int j =0; j < friends.length; j++) {
+                if(i == j) continue;
+                if (exchange[i][j] > exchange[j][i]) cnt++;
+                else if (exchange[j][i] == exchange[i][j] && count[i] > count[j] ) cnt++;
+            }
+            answer = Math.max(answer, cnt);
+        }
+            return answer;
     }
+
 }
